1b00b6ec5e0e58d903549723ae8b6aa2b910d449
Ahmed-Zarrad <73597577+Ahmed-Zarrad@users.noreply.github.com>
2026-02-11 18:49:24 +0100

    UserRepository commit

---
+++ src/main/java/lsmsdb/unipi/it/virtualtrade/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package lsmsdb.unipi.it.virtualtrade.repository;
+
+import lsmsdb.unipi.it.virtualtrade.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
