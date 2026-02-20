80873f81fa71c18af39f26e7f7ebe38b6c8e71fa
Ahmed-Zarrad <73597577+Ahmed-Zarrad@users.noreply.github.com>
2026-02-20 23:10:33 +0100

    Delete classes for merge purposes

--- src/main/java/lsmsdb/unipi/it/virtualtrade/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package lsmsdb.unipi.it.virtualtrade.repository;
-
-import lsmsdb.unipi.it.virtualtrade.model.Transaction;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.List;
-
-public interface TransactionRepository extends MongoRepository<Transaction, String> {
-    List<Transaction> findByPortfolioId(String portfolioId);
-}
