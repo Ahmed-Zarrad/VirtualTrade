c0e4464efbf5005705b588665172b68edc36d405
Ahmed-Zarrad <73597577+Ahmed-Zarrad@users.noreply.github.com>
2026-02-23 23:51:12 +0100

    Update

--- src/main/java/lsmsdb/unipi/it/virtualtrade/service/StockPriceCacheService.java
+++ src/main/java/lsmsdb/unipi/it/virtualtrade/service/StockPriceCacheService.java
@@ -3,30 +3,21 @@ package lsmsdb.unipi.it.virtualtrade.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-import java.time.Instant;
-import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class StockPriceCacheService {
 
-    private final RedisTemplate<String, Object> redisTemplate;
+    private final RedisTemplate<String, String> redisTemplate;
 
-
-    private static final String KEY_PREFIX = "stock:price:";
-
+    private static final String HASH_KEY = "prices";
 
     public void cachePrice(String symbol, Double price) {
-        String key = KEY_PREFIX + symbol;
-
-        redisTemplate.opsForValue().set(key, price, 5, TimeUnit.MINUTES);
+        redisTemplate.opsForHash().put(HASH_KEY, symbol, price.toString());
     }
 
-
     public Double getCachedPrice(String symbol) {
-        String key = KEY_PREFIX + symbol;
-        Object price = redisTemplate.opsForValue().get(key);
-
+        Object price = redisTemplate.opsForHash().get(HASH_KEY, symbol);
         if (price != null) {
             return Double.valueOf(price.toString());
         }
